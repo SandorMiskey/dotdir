c65733decc68d4f6e879ca180fa80633cbee64a0
Sandor Miskey <s@ndor.miskey.net>
2019-01-18 16:54:26 +0100

    history updates

--- zsh/oh-my-zsh-custom/lib/history.zsh
+++ zsh/oh-my-zsh-custom/lib/history.zsh
@@ -37,5 +37,6 @@ setopt hist_expire_dups_first # delete duplicates first when HISTFILE size excee
 setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
-setopt inc_append_history     # add commands to HISTFILE in order of execution
+#setopt inc_append_history     # add commands to HISTFILE in order of execution
+setopt APPEND_HISTORY
 setopt share_history          # share command history data
